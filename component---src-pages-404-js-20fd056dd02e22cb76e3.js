85fb006aa4773b8158aa1a9081adcd33b6322d0f
Jakub Kottnauer <jk@jakubkottnauer.com>
2019-09-09 15:35:35 +0200

    Updates

---
+++ component---src-pages-404-js-20fd056dd02e22cb76e3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{206:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(204),o=n(102),s=n.n(o);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(s.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-20fd056dd02e22cb76e3.js.map
