76ce129944b5609cc7682392acaf5f1a659a5aab
Jakub Kottnauer <jk@jakubkottnauer.com>
2022-05-21 13:10:39 +0200

    Updates

---
+++ component---src-pages-404-js-3a5521ada6cfcbfdaa52.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreci=self.webpackChunkreci||[]).push([[883],{429:function(e,t,n){n.r(t);var u=n(7294),l=n(5592),r=n(5414);t.default=function(){return u.createElement(l.Z,null,u.createElement(r.q,{title:"404: Not found"}),u.createElement("h1",null,"NOT FOUND"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-3a5521ada6cfcbfdaa52.js.map
